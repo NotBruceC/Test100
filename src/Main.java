f1eea941732c20f2e84165e4d3b67f531aee3447
NotBruceC <brucec2026@student.cis.edu.hk>
2023-09-06 11:19:35 +0800

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("i am under water" + "help");
+        System.out.print("i am under water" + "help");
 
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
